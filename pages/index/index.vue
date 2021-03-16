ac8fd27834365f6ba1f61a742837ada078a4b8d9
gbm <571974734@qq.com>
2021-03-16 17:52:31 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="indexBox">
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+</style>
